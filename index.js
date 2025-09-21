c67e59f95a1f3204a889718546ebeced0c03ee64
jakubznojekstabilomax-rgb <jakub.znojek.stabilomax@gmail.com>
2025-09-21 16:44:21 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  win.loadURL('https://poss.ct8.pl'); // PAMIĘTAJ: ZMIEŃ TO NA ADRES SWOJEJ STRONY
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
