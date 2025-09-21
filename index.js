a252d78d314fa7d64db73bba2ec3cb9844401477
jakubznojekstabilomax-rgb <jakub.znojek.stabilomax@gmail.com>
2025-09-21 20:29:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
 const { app, BrowserWindow, session } = require('electron');
+const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
-  });
-
-  win.webContents.session.on('select-bluetooth-device', (event, deviceList, callback) => {
-    event.preventDefault();
-
-    // Możesz tutaj stworzyć okno dialogowe, aby pokazać listę urządzeń
-    // i pozwolić użytkownikowi wybrać, z którym chce się połączyć.
-    // Na razie, na potrzeby testów, wybierzemy pierwsze urządzenie.
-    if (deviceList.length > 0) {
-        callback(deviceList[0].deviceId);
-    } else {
-        // Jeśli nie ma urządzeń, poinformuj o tym
-        callback(''); // Brak urządzenia
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true,
+      contextIsolation: false,
+      webSecurity: false,
+      bluetooth: true
     }
   });
 
+  // Dodaje funkcję obsługującą zdarzenia związane z Bluetooth
+  win.webContents.session.on('select-bluetooth-device', (event, deviceList, callback) => {
+    event.preventDefault();
+
+    if (deviceList.length > 0) {
+      // W normalnej aplikacji, tutaj wyświetliłbyś okno dialogowe z listą urządzeń.
+      // Na razie, na potrzeby testów, wybieramy pierwsze urządzenie.
+      callback(deviceList[0].deviceId);
+    } else {
+      callback(''); // Informacja o braku urządzeń
+    }
+  });
+
+  // Załadowanie Twojej strony internetowej. Pamiętaj, aby zmienić adres URL!
   win.loadURL('https://poss.ct8.pl');
 }
 
