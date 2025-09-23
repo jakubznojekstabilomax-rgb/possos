96b8229f97de33ac7be9c736fffbbd72a13a25d3
jakubznojekstabilomax-rgb <jakub.znojek.stabilomax@gmail.com>
2025-09-23 13:22:12 +0200

    Update preload.js

--- preload.js
+++ preload.js
@@ -2,6 +2,7 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld(
     'electronAPI', {
-        connectToBluetoothDevice: () => ipcRenderer.invoke('connect-to-bluetooth'),
+        connectToBluetoothDevice: () => ipcRenderer.invoke('connect-to-bluetooth-device'),
+        printReceipt: (data) => ipcRenderer.invoke('print-receipt', data)
     }
 );
